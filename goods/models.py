9624e15d19eedf0bbc5e4070fecd54c1f07f7028
Sergey Petrunin <halfstrik@gmail.com>
2014-06-05 15:02:30 +0400

    Added two models and register its in admin. TODO: add Image and Price fields.

--- goods/models.py
+++ goods/models.py
@@ -1,1 +1,24 @@
-# Create your models here.
+from django.db import models
+
+
+class Stuff(models.Model):
+    class Meta:
+        db_table = u'stuffs'
+
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __unicode__(self):
+        return u'%s (%s)' % (self.name, self.description)
+
+
+class Property(models.Model):
+    class Meta:
+        db_table = u'properties'
+        verbose_name_plural = u'properties'
+
+    value = models.CharField(max_length=200)
+    stuff = models.OneToOneField(Stuff)
+
+    def __unicode__(self):
+        return u'%s - %s' % (self.value, self.stuff)
--- goods/tests.py
+++ goods/tests.py
@@ -1,1 +1,17 @@
-# Create your tests here.
+# coding=utf-8
+
+from django.utils import unittest
+from goods.models import Stuff, Property
+
+
+class StuffTestCase(unittest.TestCase):
+    def test_unicode(self):
+        stuff = Stuff(name=u'Стакан', description=u'Хороший стакан')
+        self.assertEqual(u'Стакан (Хороший стакан)', unicode(stuff))
+
+
+class PropertyTestCase(unittest.TestCase):
+    def test_unicode(self):
+        stuff = Stuff(name=u'Стакан', description=u'Хороший стакан')
+        prop = Property(value=u'Хрустальный', stuff=stuff)
+        self.assertEqual(u'Хрустальный - Стакан (Хороший стакан)', unicode(prop))
