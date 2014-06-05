ce767452d917b63fdb9b830663c8aa6e3e5e8bb7
Sergey Petrunin <halfstrik@gmail.com>
2014-06-05 18:36:47 +0400

    Encapsulate url and search parameters inside update function.

--- static/js/stuff.js
+++ static/js/stuff.js
@@ -1,19 +1,22 @@
-/*global $,ko*/
+/*global $,ko,window*/
 var SHOP = SHOP || {};
 SHOP.stuff = SHOP.stuff || (function () {
     'use strict';
     $(function () {
         var stuffViewModel;
         stuffViewModel = {
-            stuffs: ko.observableArray()
+            stuffs: ko.observableArray(),
+            update: function () {
+                $.getJSON('/goods/stuff' + window.location.search, function (json_data) {
+                    var i;
+                    stuffViewModel.stuffs.removeAll();
+                    for (i = 0; i < json_data.length; i += 1) {
+                        stuffViewModel.stuffs.push(json_data[i]);
+                    }
+                });
+            }
         };
         ko.applyBindings(stuffViewModel);
-        $.getJSON("/goods/stuff", function (json_data) {
-            var i;
-            stuffViewModel.stuffs.removeAll();
-            for (i = 0; i < json_data.length; i += 1) {
-                stuffViewModel.stuffs.push(json_data[i]);
-            }
-        });
+        stuffViewModel.update();
     });
 }());
